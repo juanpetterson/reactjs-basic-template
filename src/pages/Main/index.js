d48b768db1b1a334c4a786d729ab105a8732a27c
Juan Petterson <juan.petterson@gmail.com>
2019-06-22 16:07:52 -0300

    First commit

---
+++ src/pages/Main/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+import { Container } from './styles';
+
+export default class Main extends Component {
+  componentDidMount() {}
+
+  render() {
+    return <Container>Hello World</Container>;
+  }
+}
---
+++ src/routes.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Switch, Route } from 'react-router-dom';
+
+import Main from './pages/Main';
+
+const Routes = () => (
+  <Switch>
+    <Route exact path="/" component={Main} />
+  </Switch>
+);
+
+export default Routes;
